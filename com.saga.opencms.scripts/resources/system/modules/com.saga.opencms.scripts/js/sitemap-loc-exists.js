71287dff5759bb15de6cae21301906199dec9195
Jesús <jesus.gregorio@sagasoluciones.com>
2016-11-23 16:10:18 +0100

    [Scripts] Creamos script SitemapLocExists.groovy

---
+++ com.saga.opencms.scripts/resources/system/modules/com.saga.opencms.scripts/js/sitemap-loc-exists.js
@@ -0,0 +1,39 @@
+/**
+ * Check if urls contained by <loc> tag exists. Url must be in the current site in order to avoid CORS error.
+ */
+function sitemapLocExists(){
+
+    /**
+     * Check if url exists
+     * @param url
+     */
+    function getUrl(url) {
+        console.log("getUrl", url);
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            console.log("readyState", this.readyState);
+            console.log("status", this.status);
+            if (this.readyState == 4 && this.status == 200) {
+                console.log("responseText", this.responseText);
+            }
+        };
+        xhttp.open("GET", url, true);
+        xhttp.send();
+    }
+
+    // Find locs
+    var locs = document.getElementsByTagName("loc");
+    if (locs.length === 0) {
+        console.warn("Tag 'loc' not found");
+    } else {
+
+        // Check url for each one
+        for (var i = 0; i < locs.length; i++) {
+            var loc = locs[i];
+            var link = loc.innerHTML;
+            getUrl(link);
+        }
+    }
+
+
+}
