913718c155c2d2b9581aa0517a70530b287b6359
Jesús <jesus.gregorio@sagasoluciones.com>
2017-11-29 18:23:13 +0100

    [UTIL] SgZip zip and unzip utils

--- com.saga.opencms.util/src/main/groovy/com/saga/opencms/util/SgZip.groovy
+++ com.saga.opencms.util/src/main/groovy/com/saga/opencms/util/SgZip.groovy
@@ -1,5 +1,7 @@
 package com.saga.opencms.util
 
+import org.apache.commons.io.FilenameUtils
+
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
 import java.util.zip.ZipOutputStream
@@ -8,6 +10,19 @@ import java.util.zip.ZipOutputStream
  * Created by jgregorio on 29/11/2017.
  */
 class SgZip {
+
+    /**
+     * Compress file or directry to zip
+     *
+     * @param source path to File or directory to zip
+     * @throws IOException
+     */
+    public static void zip(String source) throws IOException {
+        File fileToZip = new File(source);
+        String destName = FilenameUtils.removeExtension(fileToZip.getName()) + ".zip";
+        zip(source, fileToZip.getParent() + "/" + destName);
+    }
+
     /**
      * Compress file or directry to zip
      *
@@ -19,20 +34,30 @@ class SgZip {
         FileOutputStream fos = new FileOutputStream(zipPath);
         ZipOutputStream zipOut = new ZipOutputStream(fos);
         File fileToZip = new File(source);
-        zip(fileToZip, fileToZip.getName(), zipOut);
+        zip(fileToZip, zipOut);
         zipOut.close();
         fos.close();
     }
 
     /**
-     * Add zip files recursively including folders
+     * Zip file into out
      *
      * @param fileToZip
-     * @param fileName
      * @param zipOut
      * @throws IOException
      */
-    public static void zip(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException {
+    public static void zip(File fileToZip, ZipOutputStream zipOut) throws IOException {
+        zip(fileToZip, fileToZip.getName(), zipOut);
+    }
+
+    /**
+     * Zip files recursively including folders
+     *
+     * @param fileToZip
+     * @param zipOut
+     * @throws IOException
+     */
+    public static void zip(File fileToZip, String zipEntryName, ZipOutputStream zipOut) throws IOException {
         if (fileToZip.isHidden()) {
             // avoid hidden files
             return;
@@ -40,12 +65,12 @@ class SgZip {
             // zip all files contained into directory
             File[] children = fileToZip.listFiles();
             for (File childFile : children) {
-                zip(childFile, fileName + "/" + childFile.getName(), zipOut);
+                zip(childFile, zipEntryName + "/" + childFile.getName(), zipOut);
             }
         } else {
             // zip single file
             FileInputStream fis = new FileInputStream(fileToZip);
-            ZipEntry zipEntry = new ZipEntry(fileName);
+            ZipEntry zipEntry = new ZipEntry(zipEntryName);
             zipOut.putNextEntry(zipEntry);
             byte[] bytes = new byte[1024];
             int length;
@@ -58,15 +83,33 @@ class SgZip {
     }
 
     /**
+     * Zip multiples files or directories
+     *
+     * @param srcPaths
+     * @param zipPath
+     * @throws IOException
+     */
+    public static void zip(List<String> srcPaths, String zipPath) throws IOException {
+        FileOutputStream fos = new FileOutputStream(zipPath);
+        ZipOutputStream zipOut = new ZipOutputStream(fos);
+        for (String srcPath : srcPaths) {
+            File fileToZip = new File(srcPath);
+            zip(fileToZip, zipOut);
+        }
+        zipOut.close();
+        fos.close();
+    }
+
+    /**
      * Unzip file
      *
      * @param sourcePath path to zip file
-     * @param destDirectory path to directory (keep zip files)
+     * @param unzipDir path to directory (keep zip files)
      * @throws IOException
      */
-    public static void unzip(String sourcePath, String destDirectory) throws IOException {
+    public static void unzip(String sourcePath, String unzipDir) throws IOException {
         // Ensure directory exists
-        String baseDir = destDirectory;
+        String baseDir = unzipDir;
         if (!baseDir.endsWith("/")) {
             baseDir += "/";
         }
