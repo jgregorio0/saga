23fff915e658621d9d9d8ec0d3a4fb0147209fa9
jgregorio <jesus.gregorio@sagasoluciones.com>
2018-07-20 12:47:55 +0200

    [SCRIPTS] SgFile: creamos clase SgFileLarge para tratar ficheros con gran cantidad de datos a través de buffer.

--- com.saga.opencms.util/src/main/groovy/com/saga/opencms/util/SgFile.groovy
+++ com.saga.opencms.util/src/main/groovy/com/saga/opencms/util/SgFile.groovy
@@ -2,7 +2,11 @@ package com.saga.opencms.util
 
 import org.apache.commons.io.IOUtils
 import org.apache.commons.logging.Log
+import org.opencms.file.CmsFile
+import org.opencms.file.CmsObject
 import org.opencms.main.CmsLog
+import org.opencms.xml.content.CmsXmlContent
+import org.opencms.xml.content.CmsXmlContentFactory
 
 import javax.servlet.ServletOutputStream
 import javax.servlet.http.HttpServletResponse
@@ -89,5 +93,97 @@ public class SgFile {
 				sc.close();
 			}
 		}
+	}
+
+	public class SgFileLarge {
+        private final Log LOG = CmsLog.getLog(SgFileLarge.class);
+
+        File file;
+        BufferedWriter bw;
+        boolean isBufferClose;
+
+        public void initTmpBufferedFile(String preffix, String suffix, String dir, String encoding) throws Exception {
+            if (file != null || bw != null) {
+                file = null;
+                closeBuffer();
+            }
+            file = File.createTempFile(preffix, suffix, new File(dir));
+            OutputStreamWriter ow = new OutputStreamWriter(new FileOutputStream(file, true), encoding);
+            bw = new BufferedWriter(ow);
+            isBufferClose = false;
+        }
+
+        public void initBufferedFile(String absFilePath, String encoding) throws Exception {
+            if (file != null || bw != null) {
+                file = null;
+                closeBuffer();
+            }
+            file = new File(absFilePath);
+            file.createNewFile();
+            OutputStreamWriter ow = new OutputStreamWriter(new FileOutputStream(file, true), encoding);
+            bw = new BufferedWriter(ow);
+            isBufferClose = false;
+        }
+
+        public void appendBuffer(String textToAppend) throws IOException {
+            bw.write(textToAppend);
+        }
+
+        public void closeBuffer() {
+            try {
+                bw.close();
+                isBufferClose = true;
+            } catch (IOException e) {
+                LOG.error("SgFileLarge.closeBuffer", e);
+            }
+        }
+
+        public boolean rmBufferedFile() {
+            return file.delete();
+        }
+
+        public String getFileAbsolutePath() {
+            return file.getAbsolutePath();
+        }
+
+        public String getFileName() {
+            return file.getName();
+        }
+
+        public void readBuffered(String encoding){
+            FileInputStream inputStream = null;
+            Scanner sc = null;
+            try {
+                inputStream = new FileInputStream(file);
+                sc = new Scanner(inputStream, encoding);
+                int counter = 0;
+                while (sc.hasNextLine()) {
+                    counter++;
+                    String line = sc.nextLine();
+                    doEachLine(line, counter);
+                }
+                // note that Scanner suppresses exceptions
+                if (sc.ioException() != null) {
+                    sc.ioException().printStackTrace();
+                }
+            } catch (FileNotFoundException e) {
+                LOG.error("SgFileLarge.readBuffered", e)
+            } finally {
+                if (inputStream != null) {
+                    try {
+                        inputStream.close();
+                    } catch (IOException e) {
+                        LOG.error("SgFileLarge.readBuffered", e)
+                    }
+                }
+                if (sc != null) {
+                    sc.close();
+                }
+            }
+        }
+
+        protected void doEachLine(String line, int counter){
+            // TODO overwrite
+        }
 	}
 }
