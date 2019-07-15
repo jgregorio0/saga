bf924b8087a102ad504939839514d82c9dab9537
jgregorio <jesus.gregorio@sagasoluciones.com>
2019-07-15 12:06:19 +0200

    [UTIL] SgFile: codigo para convertir fichero de memoria en array de bytes

--- com.saga.opencms.util/src/main/groovy/com/saga/opencms/util/SgFile.groovy
+++ com.saga.opencms.util/src/main/groovy/com/saga/opencms/util/SgFile.groovy
@@ -2,11 +2,7 @@ package com.saga.opencms.util
 
 import org.apache.commons.io.IOUtils
 import org.apache.commons.logging.Log
-import org.opencms.file.CmsFile
-import org.opencms.file.CmsObject
 import org.opencms.main.CmsLog
-import org.opencms.xml.content.CmsXmlContent
-import org.opencms.xml.content.CmsXmlContentFactory
 
 import javax.servlet.ServletOutputStream
 import javax.servlet.http.HttpServletResponse
@@ -14,88 +10,88 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 public class SgFile {
-	private static final Log LOG = CmsLog.getLog(SgFile.class);
+    private static final Log LOG = CmsLog.getLog(SgFile.class);
 
-	public InputStream find (Class clazz, String path) throws URISyntaxException, IOException {
-		return Files.newInputStream(Paths.get(clazz.getClassLoader().getResource(path).toURI()));
-	}
+    public InputStream find(Class clazz, String path) throws URISyntaxException, IOException {
+        return Files.newInputStream(Paths.get(clazz.getClassLoader().getResource(path).toURI()));
+    }
 
-	public void upload(OutputStream out, byte[] bytes) throws IOException {
-		IOUtils.write(bytes, out);
-		out.close();
-	}
+    public void upload(OutputStream out, byte[] bytes) throws IOException {
+        IOUtils.write(bytes, out);
+        out.close();
+    }
 
-	public void upload(File file, byte[] bytes) throws IOException {
-		BufferedOutputStream out = new BufferedOutputStream(
-				new FileOutputStream(file));
-		upload(out, bytes);
-	}
+    public void upload(File file, byte[] bytes) throws IOException {
+        BufferedOutputStream out = new BufferedOutputStream(
+                new FileOutputStream(file));
+        upload(out, bytes);
+    }
 
-	public void upload(String path, byte[] bytes) throws IOException {
-		File file = new File(path);
-		upload(file, bytes);
-	}
+    public void upload(String path, byte[] bytes) throws IOException {
+        File file = new File(path);
+        upload(file, bytes);
+    }
 
-	public void upload(String folder, String filename, byte[] bytes) throws IOException {
-		makeDir(folder);
-		File file = new File(folder + File.separator + filename);
-		upload(file, bytes);
-	}
+    public void upload(String folder, String filename, byte[] bytes) throws IOException {
+        makeDir(folder);
+        File file = new File(folder + File.separator + filename);
+        upload(file, bytes);
+    }
 
-	public boolean makeDir(String path) throws IOException {
-		File dir = new File(path);
-		return dir.mkdirs();
-	}
+    public boolean makeDir(String path) throws IOException {
+        File dir = new File(path);
+        return dir.mkdirs();
+    }
 
-	public String catalinaHomePath(){
-		return System.getProperty("catalina.home");
-	}
+    public String catalinaHomePath() {
+        return System.getProperty("catalina.home");
+    }
 
-	public void downloadFile(
-			HttpServletResponse response, String contentType,
-			String contentDisposition, InputStream inputStream)
-			throws IOException {
+    public void downloadFile(
+            HttpServletResponse response, String contentType,
+            String contentDisposition, InputStream inputStream)
+            throws IOException {
 
-		final ServletOutputStream outputStream = response.getOutputStream();
-		response.setContentType(contentType);
+        final ServletOutputStream outputStream = response.getOutputStream();
+        response.setContentType(contentType);
 //                response.setHeader("Content-Disposition", "attachment;filename=\"" + e.getName().replaceAll("\\s+", " ").replaceAll("\\s+", "_") + "\"");
-		response.setHeader("Content-Disposition", contentDisposition);
-		IOUtils.copy(inputStream, outputStream);
-		outputStream.flush();
-		outputStream.close();
-	}
+        response.setHeader("Content-Disposition", contentDisposition);
+        IOUtils.copy(inputStream, outputStream);
+        outputStream.flush();
+        outputStream.close();
+    }
 
-	public void readBuffered(String filepath, String encoding){
-		def lines = [];
+    public void readBuffered(String filepath, String encoding) {
+        def lines = [];
 
-		FileInputStream inputStream = null;
-		Scanner sc = null;
-		try {
-			inputStream = new FileInputStream(filepath);
-			sc = new Scanner(inputStream, encoding);
-			int lineCounter = 0;
-			def headers = [];
-			while (sc.hasNextLine()) {
-				String line = sc.nextLine();
-				// TODO process data
-				lines.add(line);
-				lineCounter++;
-			}
-			// note that Scanner suppresses exceptions
-			if (sc.ioException() != null) {
-				LOG.error("Loading data from csv " + filepath, sc.ioException());
-			}
-		} finally {
-			if (inputStream != null) {
-				inputStream.close();
-			}
-			if (sc != null) {
-				sc.close();
-			}
-		}
-	}
+        FileInputStream inputStream = null;
+        Scanner sc = null;
+        try {
+            inputStream = new FileInputStream(filepath);
+            sc = new Scanner(inputStream, encoding);
+            int lineCounter = 0;
+            def headers = [];
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                // TODO process data
+                lines.add(line);
+                lineCounter++;
+            }
+            // note that Scanner suppresses exceptions
+            if (sc.ioException() != null) {
+                LOG.error("Loading data from csv " + filepath, sc.ioException());
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
 
-	public class SgFileLarge {
+    public class SgFileLarge {
         private final Log LOG = CmsLog.getLog(SgFileLarge.class);
 
         File file;
@@ -150,7 +146,7 @@ public class SgFile {
             return file.getName();
         }
 
-        public void readBuffered(String encoding){
+        public void readBuffered(String encoding) {
             FileInputStream inputStream = null;
             Scanner sc = null;
             try {
@@ -182,8 +178,22 @@ public class SgFile {
             }
         }
 
-        protected void doEachLine(String line, int counter){
+        protected void doEachLine(String line, int counter) {
             // TODO overwrite
         }
-	}
+    }
+
+    static byte[] fileToByteArray(File file) {
+        FileInputStream fis = new FileInputStream(file);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] buf = new byte[1024];
+        for (int readNum; (readNum = fis.read(buf)) != -1;) {
+            //Writes to this byte array output stream
+            bos.write(buf, 0, readNum);
+        }
+        byte[] bytes = bos.toByteArray();
+        return bytes;
+    }
+
+
 }
